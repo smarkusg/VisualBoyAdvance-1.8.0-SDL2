550e30db3b162392eb535c3c1925e92117e5d47a
smarkusg <smarkusg@gmail.com>
2024-06-08 21:53:58 +0200

    init

---
+++ src/sdl/amigaos.h
@@ -0,0 +1,13 @@
+/*
+** smarkusg: part of the code comes from mplayer 1.5 AOS
+** License: GPL v2 or later
+*/
+
+#include <exec/types.h>
+
+#define AMIGA_VERSION "VisualBoyAdvance 1.8.0 (8.06.2024)"
+
+int AmigaOS_Open(int argc, char *argv[]); // returns -1 if a problem
+void AmigaOS_Close(void);
+void AmigaOS_ParseArg(int argc, char *argv[], int *new_argc, char ***new_argv);
+void VARARGS68K EasyRequester(CONST_STRPTR text, CONST_STRPTR button, ...);
